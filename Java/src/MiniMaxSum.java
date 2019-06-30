aa27595e386596945d3e0f8e35138f70ea018d59
Vitalii <besteffects@gmail.com>
2019-06-30 14:30:07 -0400

    MiniMaxSum_Initial_commit

---
+++ Java/src/MiniMaxSum.java
@@ -0,0 +1,39 @@
+/*
+Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the
+five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+ */
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class MiniMaxSum {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
