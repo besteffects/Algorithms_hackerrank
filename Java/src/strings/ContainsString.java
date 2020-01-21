d6c00cbaf71835500391854ec81aae737380817a
Vit <besteffects@gmail.com>
2020-01-21 01:44:59 -0500

    ContainsString

---
+++ Java/src/strings/ContainsString.java
@@ -0,0 +1,40 @@
+package strings;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+//Print YES if string contains word "hackerrank" and NO if not
+public class ContainsString {
+
+    // Complete the hackerrankInString function below.
+    static String hackerrankInString(String s) {
+        String result="";
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            String result = hackerrankInString(s);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
