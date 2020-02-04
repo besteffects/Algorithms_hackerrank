c20018a2dea1f4ba1b741895dc44460c5579e4bf
Vit <besteffects@gmail.com>
2020-02-04 02:51:46 -0500

    InsertionSortTwo. p2

--- Java/src/sorting/InsertionSortTwo.java
+++ Java/src/sorting/InsertionSortTwo.java
@@ -58,5 +58,41 @@ Print arr
 Test position 5 against positions 4,3,2,1,0, insert 2 at position 1 and move others to the right.
 Print arr.
  */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class InsertionSortTwo {
+
+    // Complete the insertionSort2 function below.
+    static void insertionSort2(int n, int[] arr) {
+
+
+    }
+
+  //  private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+       // int n = scanner.nextInt();
+       // scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        //int[] arr = new int[n];
+
+       // String[] arrItems = scanner.nextLine().split(" ");
+       // scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+       // for (int i = 0; i < n; i++) {
+       //     int arrItem = Integer.parseInt(arrItems[i]);
+       //     arr[i] = arrItem;
+       // }
+int n =6;
+int [] arr = new int[]{1,4,3,5,6,2};
+        insertionSort2(n, arr);
+
+      //  scanner.close();
+    }
 }
