979f7ade2128a845d4a5ae17a99b0302fc32e2dd
Vit <besteffects@gmail.com>
2020-02-10 03:31:36 -0500

    LoopInvariant task. Fixed error

--- Java/src/sorting/insertionSort/LoopInvariant.java
+++ Java/src/sorting/insertionSort/LoopInvariant.java
@@ -52,7 +52,7 @@ public class LoopInvariant {
         for (int i = 1; i < A.length; i++) {
             int value = A[i];
             int j = i - 1;
-            while (j > 0 && A[j] > value) {
+            while (j >= 0 && A[j] > value) { // here was the error
                 A[j + 1] = A[j];
                 j = j - 1;
             }
