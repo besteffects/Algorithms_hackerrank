53713184a48e0be1d8ee3bd7d5f185f5ab5652b7
Vit <besteffects@gmail.com>
2020-02-17 13:24:58 -0500

    RunningTime of insertionSort. Solution

--- Java/src/sorting/insertionSort/RunningTime.java
+++ Java/src/sorting/insertionSort/RunningTime.java
@@ -38,26 +38,37 @@ Iteration   Array      Shifts
 
 Total                     4
  */
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 public class RunningTime {
 
     // Complete the runningTime function below.
     static int runningTime(int[] arr) {
-return 1;
+        int counter = 0;
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            /*
+            Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+                counter++;
+            }
+            arr[j + 1] = key;
+        }
 
+        return counter;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        // BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
@@ -73,11 +84,12 @@ return 1;
         }
 
         int result = runningTime(arr);
+        System.out.println(result);
 
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
+        //  bufferedWriter.write(String.valueOf(result));
+        //  bufferedWriter.newLine();
 
-        bufferedWriter.close();
+        //  bufferedWriter.close();
 
         scanner.close();
     }
