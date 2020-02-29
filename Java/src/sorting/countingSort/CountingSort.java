bfca2035a79a381f07e5678203035360e3eff044
Vit <besteffects@gmail.com>
2020-02-29 03:06:40 -0500

    CountingSort. Solution

--- Java/src/sorting/countingSort/CountingSort.java
+++ Java/src/sorting/countingSort/CountingSort.java
@@ -50,5 +50,62 @@ Explanation
 Each of the resulting values  represents the number of times  appeared in .
 
  */
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class CountingSort {
+
+    // Complete the countingSort function below.
+    static int[] countingSort(int[] arr) {
+        int count[] = new int[100];
+        for (int i = 0; i < 100; i++) {
+            count[i] = 0;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+        return count;
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int[] result = countingSort(arr);
+        System.out.print(Arrays.toString(result));
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        // bufferedWriter.newLine();
+
+        // bufferedWriter.close();
+
+        scanner.close();
+    }
 }
+
