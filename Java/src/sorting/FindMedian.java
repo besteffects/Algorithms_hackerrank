b4d96e6cad7a06bae2961bbffa43ee047df57d32
Vit <besteffects@gmail.com>
2020-07-20 00:36:07 -0400

    The median of a list of numbers is essentially it's middle element after sorting. The same number of elements occur after it as before. Given a list of numbers with an odd number of elements, can you find the median?

---
+++ Java/src/sorting/FindMedian.java
@@ -0,0 +1,40 @@
+package sorting;
+/*
+The median of a list of numbers is essentially it's middle element after sorting. The same number of elements occur
+after it as before. Given a list of numbers with an odd number of elements, can you find the median?
+For example, the median of arr=[1,2,3,4,5] is 3, the middle element in the sorted array.
+
+Function Description
+Complete the findMedian function in the editor below. It must return an integer that represents the median of the array.
+findMedian has the following parameter(s):
+arr: an unsorted array of integers
+
+Input Format
+The first line contains the integer n, the size of arr.
+The second line contains n space-separated integers arr[i]
+
+Constraints
+
+n is odd
+Output Format
+Output one integer, the median.
+
+Sample Input 0
+7
+0 1 2 4 6 5 3
+
+Sample Output 0
+3
+Explanation 0
+
+The sorted arr=[0,1,2,3,4,5]. It's middle element is at arr[3]=3.
+ */
+public class FindMedian {
+    public static void main(String[] args) {
+
+    }
+
+    static int findMedian(int[] arr){
+
+    }
+}
